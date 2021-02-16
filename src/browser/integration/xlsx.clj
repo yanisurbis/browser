e6e3c21aadb0c8fd6d8b056d174745747cc9b928
Yanis Urbis <yanis.urbis@gmail.com>
2021-02-16 18:44:28 +0300

    read tweet ids from xlsx

---
+++ src/browser/integration/xlsx.clj
@@ -0,0 +1,18 @@
+(ns browser.integration.xlsx
+  (:require [dk.ative.docjure.spreadsheet :as ss]
+            [clojure.string :as str]))
+
+(defn tweet-url->tweet-id
+  "takes tweet url"
+  {:test (fn []
+           (assert (tweet-url->tweet-id "http://twitter.com/smashingmag/status/1361301045482315777")
+                   "1361301045482315777"))}
+  [tweet-url]
+  (-> tweet-url
+      (clojure.string/split  #"/")
+      last))
+
+(def tweet-urls (->> (ss/load-workbook "./resources/tweets1.xlsx")
+                     (ss/select-sheet "Sheet1")
+                     (ss/select-columns {:E :tweet-url})
+                     (map (comp tweet-url->tweet-id :tweet-url))))
