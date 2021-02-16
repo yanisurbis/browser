954814ff3e751ed64da09616723631fd9ba4bc5a
Yanis Urbis <yanis.urbis@gmail.com>
2021-02-16 18:19:32 +0300

    setup basic reveal for clojure inspection

--- project.clj
+++ project.clj
@@ -8,6 +8,15 @@
                  [camel-snake-kebab "0.4.2"]
                  [environ "1.2.0"]
                  [clj-http "3.12.0"]
+                 [dk.ative/docjure "1.14.0"]
                  [cheshire "5.10.0"]
-                 [clojurewerkz/elastisch "3.0.1"]]
+                 [org.clojure/core.async "1.3.610"]
+                 [clojurewerkz/elastisch "3.0.1"]
+                 ;; for reveal
+                 [nrepl,"0.8.3"]
+                 [clj-kondo,"2020.04.05"]]
+  ;;:plugins [[cider/cider-nrepl,"0.25.6"]]
+  :profiles {:reveal {:dependencies [[vlaaad/reveal "1.2.186"]]
+                      :repl-options {:nrepl-middleware [vlaaad.reveal.nrepl/middleware]}}}
   :repl-options {:init-ns browser.core})
+                 ;;:nrepl-middleware [["cider.nrepl/cider-middleware"]]})
